ff64e1e47e972cccbdcac79a30df2ef64c2de0f7
fluffymac <maya.jones@brentwood.ca>
2015-04-10 09:03:25 -0700

    new kitchen and cupboard

---
+++ src/witchinn/Kitchen.java
@@ -0,0 +1,15 @@
+package witchinn;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mayajones
+ */
+public class Kitchen {
+    
+}
