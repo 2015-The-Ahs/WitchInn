44d0bd9a74090f821906abc110645b6a06b01e34
ShaylaG <Shayla@Shaylas-MacBook-Air.local>
2015-04-29 12:23:26 -0700

    drag and drop works!!!

--- src/witchinn/Ingredient.java
+++ src/witchinn/Ingredient.java
@@ -8,6 +8,8 @@ package witchinn;
 import environment.Actor;
 import environment.Velocity;
 import images.ResourceTools;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
 
@@ -16,24 +18,43 @@ import java.awt.Point;
  * @author carateresa
  */
 public class Ingredient extends Actor {
+
     public static String TYPE_BUG = "BUG";
 
-    public static Ingredient getIngredient(String type){
-        Ingredient ingredient = new Ingredient(getImage("resources/vine.png"), new Point (10, 10), new Velocity(0, 0));
- 
+    public static Ingredient getIngredient(String type) {
+        Ingredient ingredient = new Ingredient(loadImage("resources/vine.png"), new Point(10, 10), new Velocity(0, 0));
+
         return ingredient;
     }
-    
-    public static Image getImage(String path){
-        return ResourceTools.loadImageFromResource(path);
+
+    @Override
+    public Ingredient clone() throws CloneNotSupportedException {
+        return getIngredient(this.getName());
     }
     
+    @Override
+    public void paint(Graphics graphics){
+        graphics.drawImage(getScaledImage(), getPosition().x, getPosition().y, null);
+    }
+
+    public static Image loadImage(String path) {
+        return ResourceTools.loadImageFromResource(path);
+    }
+
+    Image getScaledImage() {
+        return super.getImage().getScaledInstance(preferredSize.width, preferredSize.height, Image.SCALE_FAST);
+    }
+
+    {
+        preferredSize = new Dimension(50, 50);
+    }
+
     public Ingredient(Image image, Point position, Velocity velocity) {
         super(image, position, velocity);
     }
-    
-    
+
     private boolean visible = true;
+    private Dimension preferredSize;
 
     /**
      * @return the visible
@@ -48,6 +69,19 @@ public class Ingredient extends Actor {
     public void setVisible(boolean visible) {
         this.visible = visible;
     }
-    
-    
+
+    /**
+     * @return the preferredSize
+     */
+    public Dimension getPreferredSize() {
+        return preferredSize;
+    }
+
+    /**
+     * @param preferredSize the preferredSize to set
+     */
+    public void setPreferredSize(Dimension preferredSize) {
+        this.preferredSize = preferredSize;
+    }
+
 }
