31830f36a2b26fe941432a079f20ed87157c30ee
Cara Alscher <carateresa@MacBook-Pro.local>
2015-05-26 10:58:44 -0700

    Font and Color for Book

--- src/witchinn/Recipe.java
+++ src/witchinn/Recipe.java
@@ -8,6 +8,8 @@ package witchinn;
 import images.ResourceTools;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Color;
+import java.awt.Font;
 
 /**
  *
@@ -16,15 +18,23 @@ import java.awt.Image;
 public class Recipe {
    private Image book;
     private String text = "";
+    private String text2 = "";
+    private Color color;
     
     public Recipe(){
         book = ResourceTools.loadImageFromResource("resources/old_book.png");
         text = "swaggygswaygwysgsway";
+        text2 = "mr. lawrence can't kobe";
+        
     }
 
     void draw(Graphics graphics) {
         graphics.drawImage(book, 140, 90, null);
+        graphics.setFont(new Font("Comic Sans", Font.PLAIN, 12));
+        graphics.setColor(Color.RED);
         graphics.drawString(text, 250, 140);
+
+        
         
     }
  
