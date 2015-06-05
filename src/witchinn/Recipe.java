f6dde4faac9577398f83b0e7f5c96a7a72f8a4f0
Cara Alscher <carateresa@MacBook-Pro.local>
2015-06-05 08:40:01 -0700

    Font

--- src/witchinn/Recipe.java
+++ src/witchinn/Recipe.java
@@ -10,7 +10,12 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static witchinn.Ingredient.*;
 
 /**
@@ -19,12 +24,6 @@ import static witchinn.Ingredient.*;
  */
 public class Recipe {
 
-    private Image book;
-    private String title = "";
-    private ArrayList<Ingredient> ingredients;
-    
-    private Color color;
-
     public static final String RECIPE_ANOTHER = "Another....";
     public static final String RECIPE_VINE_SALAD = "Vine Salad";
     public static final String RECIPE_CARA = "Caramelized Cara";
@@ -32,69 +31,97 @@ public class Recipe {
     public static final String RECIPE_GEMMA = "Grits 'n Gemma";
     public static final String RECIPE_MAYA = "Minced Maya";
 
-    
-    public static Recipe getRecipe(String recipeName){
+    private static Font loadFont(String resource, float size) {
+        Font font = null;
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream input = classLoader.getResourceAsStream(resource);
+
+            font = Font.createFont(Font.TRUETYPE_FONT, input).deriveFont(size);
+        } catch (FontFormatException | IOException ex) {
+            Logger.getLogger(Recipe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return font;
+    }
+
+    public static Recipe getRecipe(String recipeName) {
         Recipe recipe = new Recipe(recipeName);
-        
-        switch (recipeName){
-            
+
+        switch (recipeName) {
+
             case RECIPE_ANOTHER:
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 break;
-            
-            
+
             default:
             case RECIPE_VINE_SALAD:
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 break;
-                
+
             case RECIPE_CARA:
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 break;
-                
+
             case RECIPE_SHAYLA:
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 break;
-            
+
             case RECIPE_GEMMA:
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 break;
-            
+
             case RECIPE_MAYA:
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 recipe.getIngredients().add(getIngredient(INGREDIENT_VINE));
                 break;
-            
+
         }
-        
+
         return recipe;
     }
-    
-    
+
+//<editor-fold defaultstate="collapsed" desc="Constructors">
     {
         setBook(ResourceTools.loadImageFromResource("resources/book.PNG").getScaledInstance(600, 430, Image.SCALE_FAST));
         setIngredients(new ArrayList<>());
-        
+
         setTitle("");
+        font = loadFont("resources/Zeyada.ttf", (float) 32.0);
     }
-    
+
     public Recipe(String title) {
         book = (ResourceTools.loadImageFromResource("resources/book.PNG").getScaledInstance(600, 430, Image.SCALE_FAST));
         this.title = title;
     }
+//</editor-fold>
 
+//<editor-fold defaultstate="collapsed" desc="Methods">
     void draw(Graphics graphics) {
         graphics.drawImage(getBook(), 140, 90, null);
-        
-        graphics.setFont(new Font("Comic Sans", Font.PLAIN, 12));
-        graphics.setColor(Color.BLUE);
-        graphics.drawString(getTitle(), 250, 140);
+
+        if (font != null) {
+            graphics.setFont(font);
+        } else {
+            graphics.setFont(new Font("Comic Sans", Font.PLAIN, 12));
+        }
+
+//        graphics.setFont(new Font("Comic Sans", Font.PLAIN, 12));
+        graphics.setColor(Color.BLACK);
+        graphics.drawString(getTitle(), 230, 200);
     }
+//</editor-fold>
+
+    private Image book;
+    private String title = "";
+    private ArrayList<Ingredient> ingredients;
+
+    private Color color;
+    private Font font;
 
     /**
      * @return the book
@@ -140,7 +167,3 @@ public class Recipe {
 
 }
 
-//Change font
-//Color of text
-//Format the ingredients (into lines)
-//Pictures
